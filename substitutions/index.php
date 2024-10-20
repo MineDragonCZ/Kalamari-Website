9bdfd3579068645df0ef4979c56185fedbf60aa5
MineDragonCZ_ <minedragoncz@email.cz>
2024-10-20 21:51:27 +0200

    Added substitutions

--- substitutions/index.php
+++ substitutions/index.php
@@ -2,20 +2,16 @@
 $pageTitle = "Suplování";
 include(dirname(__FILE__) . "/../tools/inc/page.php");
 ?>
-<div class="container-xl">
-	<div class="mx-5">
-		<div class="mb-2 alert" style="--border: #0FD1F7c5; --back: #0FD1F722; --style: none;">
-			<div class="row align-items-center">
-				<div class="col-auto ms-3">
-					<span class="text-xxl" style="color: #0FD1F7c5;"><i class="fa-solid fa-circle-info"></i></span>
-				</div>
-				<div class="col">
-					<h3>Na této stránce se pracuje!</h3>
-					<span class="card-desc">
-						Zatím můžete zkusit třeba rozvrhy
-					</span>
-				</div>
+
+<div class="mx-5">
+	<div class="col-12">
+		<div class="row justify-content-center mb-3">
+			<div class="col-6" id="dateSelectorWrapper">
+				<input type="date" id="dateSelector" onchange="dateString = this.value; updateSubstitutionTable();" class="form-control">
 			</div>
+		</div>
+		<div class="col-12" style="overflow-x: auto;">
+			<table id="substitutionTable" class="pb-5 table table-striped table-borderless" style="margin-bottom: 250px;"></table>
 		</div>
 	</div>
 </div>
@@ -24,4 +20,75 @@ include(dirname(__FILE__) . "/../tools/inc/page.php");
 
 <script>
 	setupLoginChecker();
+
+	var dateString = new Date().toISOString().split('T')[0];
+	var linkDate = getFromLink("d");
+	if(linkDate){
+		dateString = linkDate;
+	}
+
+	function updateDate(){
+		pushLink("timetable", "?d=" + dateString);
+		$("#dateSelector").val(dateString);
+	}
+
+	async function updateSubstitutionTable() {
+		updateDate();
+		var response = await fetchDataFromBAK('/api/3/substitutions/?from=' + dateString, null, "GET", true);
+		var json = await response.json();
+		console.log(json);
+
+		var table = $("#substitutionTable");
+
+		/* REMAP FROM ARRAYS TO OBJECTS */
+
+		/* PRINT INTO TABLE */
+		var content = "";
+		content += `<thead>`;
+		content += `<td></td>`;
+		content += `<td></td>`;
+		content += `<td></td>`;
+		content += `<td></td>`;
+		content += `<td></td>`;
+		content += `</thead>`;
+		for(let i = 0; i < json.Changes.length; i++){
+			var change = json.Changes[i];
+			var date = new Date(change.Day);
+			var day = `${getDayName(date.getDay(), false)} <span>(${date.toLocaleDateString()})</span>`;
+			var hourInterval = change.Hours;
+			var icon = '';
+			var timeInterval = change.Time;
+			var desc = change.Description;
+
+			switch(change.ChangeType){
+				case "Added":
+					icon = '<i class="fa-solid fa-circle-plus fa-fw" style="color: #24aa13;"></i>';
+					break;
+				case "Removed":
+					icon = '<i class="fa-solid fa-circle-minus fa-fw" style="color: #b01111;"></i>';
+					break;
+				case "Canceled":
+					icon = '<i class="fa-solid fa-circle-xmark fa-fw" style="color: #b01111;"></i>';
+					break;
+				case "Substitution":
+					icon = '<i class="fa-solid fa-repeat fa-fw" style="color: #117bb0;"></i>';
+					break;
+				default:
+					break;
+			}
+
+			if(new Date().setHours(0,0,0,0) == date.setHours(0,0,0,0))
+				content += `<tr class="bordered-row" style="--back: var(--secondary);">`;
+			else
+				content += `<tr>`;
+			content += `<td class="text-orange">${day}</td>`;
+			content += `<td class="text-orange">${hourInterval}</td>`;
+			content += `<td>${icon}</td>`;
+			content += `<td class="card-desc text-sm">${timeInterval}</td>`;
+			content += `<td>${desc}</td>`;
+			content += `</tr>`;
+		}
+		table.html(content);
+	}
+	updateSubstitutionTable();
 </script>
